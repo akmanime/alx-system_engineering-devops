0535330da3033fa4b4e1a4e0ba3932eb06a22118
akmanime <akramryad3@gmail.com>
2024-03-05 19:50:50 +0000

    api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-import sys
+"first 10 hot posts"
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        for i in range(10):
+            print(data['data']['children'][i]['data']['title'])
     else:
-        top_ten(sys.argv[1])
+        print(None)
