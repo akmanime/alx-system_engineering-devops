d96f1d6a47b76d641bbf2416f79342b9edc29419
akmanime <akramryad3@gmail.com>
2024-03-05 11:21:12 -0800

    kk

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
+"""A function that queries the Reddit API and
+returns the number of subscribers (not active users,
+total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0."""
+import requests
 
-from requests import get
+
 def number_of_subscribers(subreddit):
-if subreddit is None or not isinstance(subreddit, str):
-return 0
-user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-response = get(url, headers=user_agent)
-results = response.json()
-try:
-return results.get('data').get('subscribers')
-except Exception:
-return 0
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
